e3d457d0e3cf4ac03f122c783499d1e7ac390f21
Evan Koehler <evan.koehler@epitech.eu>
2021-06-08 19:17:09 +0200

    functionnal

--- MyBot.py
+++ MyBot.py
@@ -1,34 +1,42 @@
 import discord
 from discord import channel
+from discord.abc import _Undefined
 from discord.message import Message
 from discord.ext import commands, tasks
 from random import randint
 
-class MyBot(discord.Client):
-    async def __init__(self, bot):
-        self.bot = bot
-        self.switch = {
-            "ping": lambda: MyBot.pong,
-            "create_channel": lambda: MyBot.create_channel,
-        }
+def is_prefix(msg):
+    if msg[:2] == '::':
+        return True
+    return False
 
+class MyBot(discord.Client):
     async def on_ready(self):
-        global status
+        self.status = [
+        'Epitech 2025',
+        'Bot by Evan Koehler',
+        'The less you sleep, the stronger you are'
+        ]
+        self.switch = {
+            'ping': self.pong,
+            'create_channel': self.create_channel,
+        }
         print('Logged on as', self.user)
 
     async def pong(self, message):
         await message.channel.send('pong')
 
     async def create_channel(self, message):
-        print("You succesfully created a channel you looser")
+        message.content
+        print(f'Channel created.')
+        await message.channel.send('create_channel')
 
     async def on_message(self, message):
         if message.author == self.user or message.author.bot:
             return
-        self.switch.get(message)(message)
-
-status = [
-    'Epitech 2025',
-    'Bot by Evan Koehler',
-    'The less you sleep, the stronger you are'
-]
+        elif not is_prefix(message.content):
+            return
+        fun = self.switch.get(message.content[2:], 'invalid')
+        if fun != 'invalid':
+            return await fun(message)
+        return
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import discord
 from MyBot import MyBot
+from dotenv import load_dotenv
 import os
 
+load_dotenv('.env')
 case_insensitive = True
 
 bot = MyBot()
-bot.run(os.environ.get('SECRET'))
+bot.run(os.getenv('SECRET'))
