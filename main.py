ab0db6c76cf937c2e3257b0bed8582bf7d1d0130
Evan Koehler <evan.koehler@epitech.eu>
2021-06-08 18:32:14 +0200

    hid token in .env

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import discord
 from MyBot import MyBot
+import os
 
 case_insensitive = True
-SECRET = 'ODUxNzgyODYwOTU5NTE0NjU1.YL9Smw.O5oSOELPck2wVBlpOIXb_ZGlpAs'
 
 bot = MyBot()
-bot.run(SECRET)
+bot.run(os.environ.get('SECRET'))
