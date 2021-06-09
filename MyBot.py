899ae099be5aabfc943dc3c8e313a8e3542106ff
Evan Koehler <evan.koehler@epitech.eu>
2021-06-09 09:01:39 +0200

    it clears chat

--- MyBot.py
+++ MyBot.py
@@ -22,24 +22,13 @@ class MyBot(discord.Client):
         self.switch = {
             'ping': self.pong,
             'c_create': self.create_channel,
-            'c_delete': self.delete_channel
+            'c_clear': self.clear_channel
         }
 
         print('Logged on as', self.user)
 
     async def pong(self, ctx, msg):
         await ctx.channel.send('pong')
-
-    async def delete_channel(self, ctx, msg):
-        if len(msg) != 2:
-            return await ctx.channel.send('[c_delete] Delete a channel\n\nUSAGE:\n\t::c_delete name')
-        #channel_does_exist = discord.utils.get(ctx.guild.channels, name=msg[2])
-        channel_does_exist = discord.utils.find(lambda m: m.name == msg[2], ctx.guild.channels)
-        if channel_does_exist != None:
-            await channel_does_exist.delete()
-            print(f'Channel {msg[2]} deleted by @{ctx.author}.')
-            return await ctx.channel.send(f'Channel {msg[2]} created by @{ctx.author} ! Have fun !')
-        return await ctx.channel.send(f'The channel \'{msg[2]}\' does not exist !')
 
     async def create_channel(self, ctx, msg):
         if len(msg) != 3:
@@ -51,6 +40,14 @@ class MyBot(discord.Client):
             await ctx.guild.create_text_channel(name=msg[2])
         print(f'Channel {msg[2]} created by @{ctx.author}.')
         return await ctx.channel.send(f'Channel {msg[2]} created by @{ctx.author} ! Have fun !')
+
+    async def clear_channel(self, ctx, msg):
+        limit = 100
+        if (len(msg) > 1):
+            limit = int(msg[1])
+        await ctx.channel.purge(limit=limit)
+        print(f'@{ctx.author} cleared the chat !')
+        return await ctx.channel.send('Well, you\'ve got a nice clean chat now !')
 
     async def on_message(self, message):
         if message.author == self.user or message.author.bot:
